ad7ca31e24560a8c6663c19146ac9e0f1d55bf0e
InfinitechCorp <infinitechcorp.ph@gmail.com>
2025-05-15 13:49:57 +0800

    Update Uploadable.php

--- app/Traits/Uploadable.php
+++ app/Traits/Uploadable.php
@@ -2,7 +2,6 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 trait Uploadable
@@ -10,11 +9,18 @@ trait Uploadable
     public function upload($file, $directory)
     {
         if ($file) {
-            $name = Str::ulid() . "." . $file->clientExtension();
-            Storage::disk('s3')->put("$directory/$name", $file->getContent(), 'public');
+            $name = Str::ulid() . '.' . $file->getClientOriginalExtension();
+            $destinationPath = public_path($directory);
+
+            // Create the directory if it doesn't exist
+            if (!file_exists($destinationPath)) {
+                mkdir($destinationPath, 0755, true);
+            }
+
+            $file->move($destinationPath, $name);
             return $name;
-        } else {
-            return null;
         }
+
+        return null;
     }
 }
